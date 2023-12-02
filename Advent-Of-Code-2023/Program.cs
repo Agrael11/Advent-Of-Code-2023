2536539297d87b5a4ddf6e7b7253330f19fe5c2e
Agrael11 <Oliver.Neuschl@gmail.com>
2023-12-02 12:35:44 +0100

    Updated console with auto download

--- Advent-Of-Code-2023/Program.cs
+++ Advent-Of-Code-2023/Program.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Transactions;
@@ -16,23 +17,47 @@ namespace AdventOfCode
             DrawATree();
             Console.ForegroundColor = ConsoleColor.White;
             string result = "";
+            bool firstStart = true;
+            Console.WriteLine("Advent of Code 2023!");
             while (!result.Equals("Q", StringComparison.CurrentCultureIgnoreCase))
             {
-                Console.WriteLine("Advent of Code 2023!");
-                Console.Write("Select the challenge [1-25] or all challenges [A], write [Q] to quit: ");
+                if (firstStart)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("You can select the challenge using days number [1-25]");
+                    Console.WriteLine("You can also select to run all available challenges by using letter \"A\"");
+                    Console.WriteLine("You can delete your cached inputs by writing \"D\"");
+                    Console.WriteLine("To quit write letter \"Q\"");
+                    Console.WriteLine("To show this info, write letter \"I\"");
+                    Console.WriteLine();
+                    Console.WriteLine("This program is able to automatically download your inputs for Advent of Code");
+                    Console.WriteLine("To do that you need to provide \"cookie.txt\" file in \"Settings\" folder of program.");
+                    Console.WriteLine("To get detailed info about how to get \"cookie.txt\", use [C] option.");
+                    Console.WriteLine("Your inputs are cached in \"Inputs\" folder.");
+                    Console.WriteLine("You can also put them in manually in format \"inputData_{year}_{day}.txt\".");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White; 
+                    Console.Write("What is your choice? ");
+                    firstStart = false;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Select the challenge [1-25] or all challenges [A], write [I] for more info or [Q] to quit: ");
+                }
                 result = Console.ReadLine() ?? "";
 
                 if (int.TryParse(result, out int parsed) && parsed >= 1 && parsed <= 25)
                 {
                     if (CheckPossible(2023, parsed))
                     {
-                        DoChallenge(parsed);
+                        DoChallenge(2023, parsed);
                         Console.WriteLine();
                     }
                     else
                     {
                         DrawATree();
-                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine($"Challenge for Day {parsed} is not finished yet.");
                         Console.WriteLine();
                     }
@@ -41,11 +66,43 @@ namespace AdventOfCode
                 {
                     DoAllChallenges();
                 }
-                else if (!result.Equals("Q", StringComparison.CurrentCultureIgnoreCase))
+                else if (result.Equals("I", StringComparison.CurrentCultureIgnoreCase))
                 {
                     DrawATree();
-                    Console.ForegroundColor = ConsoleColor.White;
+                    firstStart = true;
+                }
+                else if (result.Equals("C", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    DrawATree();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("To get your Advent of Code login cookie:");
+                    Console.WriteLine("1) Open input in your browser when logged in.");
+                    Console.WriteLine("2) Use \"F12\" to open developer tools, and select \"Network\" tab");
+                    Console.WriteLine("3) Refresh the page");
+                    Console.WriteLine("4) Select \"input\"");
+                    Console.WriteLine("5) On right side, in  Headers tab, find \"Cookie\"");
+                    Console.WriteLine("6) Copy the text of a Cookie");
+                    Console.WriteLine("7) Paste the text into \"cookie.txt\" file in \"Settings\" folder of program");
+                    Console.WriteLine();
+                }
+                else if (result.Equals("D", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    DrawATree();
+                    try
+                    {
+                        Directory.Delete("Inputs", true);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                else
+                {
+                    DrawATree();
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Wrong command.");
+                    Console.WriteLine();
                 }
             }
             Console.ForegroundColor = ConsoleColor.Gray;
@@ -68,16 +125,26 @@ namespace AdventOfCode
             return time;
         }
 
-        static ulong DoChallenge(int day, bool nice = true)
+        static ulong DoChallenge(int year, int day, bool nice = true)
         {
             if (nice)
             {
                 DrawATree();
             }
             string? inputData = null;
-            if (File.Exists($"inputData{day}.txt"))
+            string filePath = Path.Combine("Inputs", $"inputData_{year}_{day}.txt");
+            if (File.Exists(filePath))
             {
-                inputData = File.ReadAllText($"inputData{day}.txt");
+                inputData = File.ReadAllText(filePath);
+            }
+            else
+            {
+                Task<DownloadState> downloadStateTask = DownloadInput(year, day);
+                downloadStateTask.Wait();
+                if (downloadStateTask.Result == DownloadState.Downloaded || downloadStateTask.Result == DownloadState.FileExists)
+                {
+                    inputData = File.ReadAllText(filePath);
+                }
             }
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write($"Results of Day {day} are: ");
@@ -124,21 +191,20 @@ namespace AdventOfCode
         {
             MethodInfo? challenge1method = GetMethodInfo(2023, day, challenge);
             string result = "";
-            if (challenge1method != null && inputData != null)
+            Stopwatch watch;
+            if (challenge1method is null || inputData is null)
             {
-                string[] data = [inputData];
-                Stopwatch watch = Stopwatch.StartNew();
-                result = challenge1method.Invoke(null, data)?.ToString() ?? "ERROR";
-                watch.Stop();
-                return (watch, result);
-            }
-            else
-            {
-                Stopwatch watch = Stopwatch.StartNew();
+                watch = Stopwatch.StartNew();
                 result = "ERROR";
                 watch.Stop();
                 return (watch, result);
             }
+
+            string[] data = [inputData];
+            watch = Stopwatch.StartNew();
+            result = challenge1method.Invoke(null, data)?.ToString() ?? "ERROR";
+            watch.Stop();
+            return (watch, result);
         }
 
         static bool CheckPossible(int year, int day)
@@ -163,7 +229,7 @@ namespace AdventOfCode
             {
                 if (CheckPossible(2023, i))
                 {
-                    totalTime += DoChallenge(i, false);
+                    totalTime += DoChallenge(2023, i, false);
                 }
                 else
                 {
@@ -209,6 +275,55 @@ namespace AdventOfCode
             DrawLine(@"//o//i\\*\\\", 6, 5);
             DrawLine(@"/i///*/\\\\\o\", 5, 3);
             DrawLine(@"||", 11, 1);
+        }
+
+        enum DownloadState { Downloaded, FileExists, FailedDownload, FailedFileWrite, NoCookie }
+        static async Task<DownloadState> DownloadInput(int year, int day)
+        {
+            if (!File.Exists(Path.Combine("Inputs", $"inputData_{year}_{day}.txt")))
+            {
+                if (!File.Exists("Settings/cookie.txt"))
+                {
+                    return DownloadState.NoCookie;
+                }
+                string cookie = File.ReadAllText("Settings/cookie.txt");
+
+                try
+                {
+                    using HttpClient client = new();
+                    client.DefaultRequestHeaders.Add("Cookie", cookie);
+                    HttpResponseMessage response = await client.GetAsync($"https://adventofcode.com/{year}/day/{day}/input");
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        byte[] data = await response.Content.ReadAsByteArrayAsync();
+                        try
+                        {
+                            if (!Directory.Exists("Inputs"))
+                            {
+                                Directory.CreateDirectory("Inputs");
+                            }
+                            File.WriteAllBytes(Path.Combine("Inputs",$"inputData_{year}_{day}.txt"), data);
+                        }
+                        catch
+                        {
+                            return DownloadState.FailedFileWrite;
+                        }
+                        return DownloadState.Downloaded;
+                    }
+                    else
+                    {
+                        return DownloadState.FailedDownload;
+                    }
+                }
+                catch
+                {
+                    return DownloadState.FailedDownload;
+                }
+            }
+            else
+            {
+                return DownloadState.FileExists;
+            }
         }
 
         static void DrawLine(string line, int left, int day)
