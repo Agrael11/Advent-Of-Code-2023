e2d1324df6262a1cbe057c52685dda084d5fe3ee
Agrael11 <Oliver.Neuschl@gmail.com>
2023-12-08 12:24:03 +0100

    Added some basic math helpers. Greatest Common Factor and LeastCommonMultiplier specifically.

---
+++ Helpers/MathHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class MathHelpers
+    {
+        public static long GreatestCommonFactor(long firstNumber, long secondNumber)
+        {
+            while (secondNumber != 0)
+            {
+                long temp = secondNumber;
+                secondNumber = firstNumber % secondNumber;
+                firstNumber = temp;
+            }
+            return firstNumber;
+        }
+
+        public static long LeastCommonMultiple(long firstNumber, long secondNumber)
+        {
+            return (firstNumber / GreatestCommonFactor(firstNumber, secondNumber)) * secondNumber;
+        }
+    }
+}
